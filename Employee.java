1cfd6e56451baa0629ad8584056440513adcdad7
shanmukh-v-i <shanmukh.v.i@gmail.com>
2021-04-20 15:53:19 +0530

    merging

---
+++ Employee.java
@@ -0,0 +1,8 @@
+public class Employee{
+static String name="Rambo";
+static int id=101;
+public static void main(String emp[]){
+System.out.println("Employee Name "+Employee.name);
+System.out.println("Employee Id: "+Employee.id);
+}
+}
