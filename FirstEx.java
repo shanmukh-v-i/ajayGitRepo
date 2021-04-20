942eba870cc03e7c741d3d2b6cf50da9d1cee646
shanmukh-v-i <35656192+shanmukh-v-i@users.noreply.github.com>
2021-04-20 13:54:44 +0530

    updated file in branch

--- FirstEx.java
+++ FirstEx.java
@@ -2,7 +2,7 @@ public class FirstEx{
 public static void main(String k[]){
 System.out.println("Git Welcome To Java");
 for(int i=0;i<10;i++){
-System.out.println(i);
+System.out.println("Bholu Ram"+i);
 }
 }
 }
