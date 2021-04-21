6d191bc2c1a610a5acc2dc6fe65e50469cd02774
shanmukh-v-i <35656192+shanmukh-v-i@users.noreply.github.com>
2021-04-21 14:15:59 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,8 +1,10 @@
 public class Employee{
 static String name="Rambo";
 static int id=101;
+  static double salary=12000.00;
 public static void main(String emp[]){
 System.out.println("Employee Name "+Employee.name);
 System.out.println("Employee Id: "+Employee.id);
+  System.out.println("Employee Salary: "+Employee.salary);
 }
 }
